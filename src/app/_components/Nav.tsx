7fd5d36e9fa2ea458c3a098080f8774b06138d41
24LP7981 24LP7981 <24LP7981@G303-CC0146.local>
2025-03-20 12:26:06 +0800

    gg

--- src/app/_components/Nav.tsx
+++ src/app/_components/Nav.tsx
@@ -1,30 +1,30 @@
-import { Film, Moon, Search } from 'lucide-react'
-import React from 'react'
-import GenreSelector from './GenreSelector'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { Props } from 'next/script'
+// import { Film, Moon, Search } from 'lucide-react'
+// import React from 'react'
+// import GenreSelector from './GenreSelector'
+// import { Input } from '@/components/ui/input'
+// import { Button } from '@/components/ui/button'
+// import { Props } from 'next/script'
 
-const Nav = (props) => {
-  return (
-    <div className="flex items-center justify-around h-24">
-        <div className="flex">
-          <Film className="text-indigo-700" />
-          <span className="text-indigo-700">Movie Z</span>
-        </div>
-        <div className="flex gap-4">
-          <GenreSelector setGenreId={setGenreId} genreId={genreId} />
-          <div className="flex items-center border border-gray-300 rounded-md">
-            <Search />
-            <Input placeholder={`Search...`} className="border-none selection:border-none" onChange={(e) => setSearchValue(e.currentTarget.value)} />
-          </div>
+// const Nav = (props) => {
+//   return (
+//     <div className="flex items-center justify-around h-24">
+//         <div className="flex">
+//           <Film className="text-indigo-700" />
+//           <span className="text-indigo-700">Movie Z</span>
+//         </div>
+//         <div className="flex gap-4">
+//           <GenreSelector setGenreId={setGenreId} genreId={genreId} />
+//           <div className="flex items-center border border-gray-300 rounded-md">
+//             <Search />
+//             <Input placeholder={`Search...`} className="border-none selection:border-none" onChange={(e) => setSearchValue(e.currentTarget.value)} />
+//           </div>
 
-        </div>
-        <Button className="bg-white border border-gray-300 text-gray-700 hover:bg-gray-50" onClick={() => { }}>
-          <Moon className="text-black" />
-        </Button>
-      </div>
-  )
-}
+//         </div>
+//         <Button className="bg-white border border-gray-300 text-gray-700 hover:bg-gray-50" onClick={() => { }}>
+//           <Moon className="text-black" />
+//         </Button>
+//       </div>
+//   )
+// }
 
-export default Nav
+// export default Nav
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,75 +13,97 @@ export type MovieTypes = {
   video: boolean;
   vote_average: number;
   vote_count: number;
-}
+};
 export type GalleryProps = {
   movieType: MovieTypes[];
   movieList: [];
-}
+};
 
-'use client'
+("use client");
 import { ChangeEvent, useState } from "react";
 import { useEffect } from "react";
 import { ACCESS_TOKEN } from "../constants";
-import axios, { isCancel, AxiosError } from 'axios';
+import axios, { isCancel, AxiosError } from "axios";
 import Gallery from "@/components/Gallery";
 import GenreSelector from "./_components/GenreSelector";
 import { Film, Moon, Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import { instance } from "./utils/axios-instance";
 import { PosterSwiper } from "./_components/PosterSwiper";
 import Nav from "./_components/Nav";
 import { Footer } from "@/components/footer";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 export default function Home() {
-  const [movieList, setMovieList] = useState([])
+  const [movieList, setMovieList] = useState([]);
   const [genreId, setGenreId] = useState(18);
-  const [searchValue, setSearchValue] = useState("")
-  const [nowPlayingMovies, setNowPLayingMovies] = useState([])
-  const [upComingMovies, setUpComingMovies] = useState([])
-  const [topRatedMovies, setTopRatedMovies] = useState([])
-  const [popularMovies, setPopularMovies] = useState([])
+  const [searchValue, setSearchValue] = useState("");
+  const [nowPlayingMovies, setNowPLayingMovies] = useState([]);
+  const [upComingMovies, setUpComingMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
   const getMovies = async () => {
-    const movies = await axios.get(`https://api.themoviedb.org/3/${(searchValue === "" ? "discover" : "search")}/movie?language=en&with_genres=${genreId}&query=${searchValue}`, {
-      headers: {
-        Authorization: `Bearer ${ACCESS_TOKEN}`
+    const movies = await axios.get(
+      `https://api.themoviedb.org/3/${
+        searchValue === "" ? "discover" : "search"
+      }/movie?language=en&with_genres=${genreId}&query=${searchValue}`,
+      {
+        headers: {
+          Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },
       }
-    })
-    setMovieList(movies.data.results)
-    console.log(movies.data.results)
+    );
+    setMovieList(movies.data.results);
+    console.log(movies.data.results);
     // const movies = await res.json();
   };
-  const getNowPlayingMovies = async()=>{
-      const nowPlayingmovies = await instance.get(`/movie/now_playing`)
-      setNowPLayingMovies(nowPlayingmovies.data.results)
-  }
-  const getUpComingMovies = async()=>{
-    const upComingMovies = await instance.get(`/movie/upcoming?language=en&with_genres=${genreId}&query=${searchValue}`)
-    setUpComingMovies(upComingMovies.data.results)
-  }
-  const getTopRatedMovies = async()=>{
-    const topRatedMovies = await instance.get(`/movie/top_rated?language=en&with_genres=${genreId}&query=${searchValue}`)
-    setTopRatedMovies(topRatedMovies.data.results)
-  }
-  const getPopularMovies = async()=>{
-    const popularMovies = await instance.get(`/movie/popular?language=en&with_genres=${genreId}&query=${searchValue}`)
-    setPopularMovies(popularMovies.data.results)
-  }
+  const getNowPlayingMovies = async () => {
+    const nowPlayingmovies = await instance.get(`/movie/now_playing`);
+    setNowPLayingMovies(nowPlayingmovies.data.results);
+  };
+  const getUpComingMovies = async () => {
+    const upComingMovies = await instance.get(
+      `/movie/upcoming?language=en&with_genres=${genreId}&query=${searchValue}`
+    );
+    setUpComingMovies(upComingMovies.data.results);
+  };
+  const getTopRatedMovies = async () => {
+    const topRatedMovies = await instance.get(
+      `/movie/top_rated?language=en&with_genres=${genreId}&query=${searchValue}`
+    );
+    setTopRatedMovies(topRatedMovies.data.results);
+  };
+  const getPopularMovies = async () => {
+    const popularMovies = await instance.get(
+      `/movie/popular?language=en&with_genres=${genreId}&query=${searchValue}`
+    );
+    setPopularMovies(popularMovies.data.results);
+  };
   useEffect(() => {
     getMovies();
     getNowPlayingMovies();
     getUpComingMovies();
     getTopRatedMovies();
     getPopularMovies();
-  }, [genreId, searchValue])
-  console.log(searchValue)
-  const movieRanks = [
-    "Popular",
-    "Top rated",
-    "Upcoming"
-  ]
+  }, [genreId, searchValue]);
+  console.log(searchValue);
+  const movieRanks = ["Popular", "Top rated", "Upcoming"];
   return (
     <div className="p-0">
       <div className="flex items-center justify-around h-24">
@@ -93,11 +115,17 @@ export default function Home() {
           <GenreSelector setGenreId={setGenreId} genreId={genreId} />
           <div className="flex items-center border border-gray-300 rounded-md">
             <Search />
-            <Input placeholder={`Search...`} className="border-none selection:border-none" onChange={(e) => setSearchValue(e.currentTarget.value)} />
+            <Input
+              placeholder={`Search...`}
+              className="border-none selection:border-none"
+              onChange={(e) => setSearchValue(e.currentTarget.value)}
+            />
           </div>
-
         </div>
-        <Button className="bg-white border border-gray-300 text-gray-700 hover:bg-gray-50" onClick={() => { }}>
+        <Button
+          className="bg-white border border-gray-300 text-gray-700 hover:bg-gray-50"
+          onClick={() => {}}
+        >
           <Moon className="text-black" />
         </Button>
       </div>
@@ -114,21 +142,43 @@ export default function Home() {
       </Carousel> */}
       <PosterSwiper></PosterSwiper>
       {/* <Gallery movieList={movieList} /> */}
-          {movieRanks.map((rank, index) => {
-            return (
-              <div key={index}
-                className="w-[1438px] mx-auto my-12"
-              >
-                <div className="flex items-center justify-between">
-                  <h1 className="text-3xl font-bold mx-20 mb-12">{rank}</h1>
-                    </div>
-                <Gallery movieList={rank === "Popular" ? popularMovies : rank === "Top rated" ? topRatedMovies : upComingMovies} />
-              </div>
-            );
-          })}
-          <Gallery movieList={movieList} />
-          <Footer></Footer>
-    </div>
+      {movieRanks.map((rank, index) => {
+        return (
+          <div key={index} className="w-[1438px] mx-auto my-12">
+            <div className="flex items-center justify-between">
+              <h1 className="text-3xl font-bold mx-20 mb-12">{rank}</h1>
+            </div>
+            <Gallery
+              movieList={
+                rank === "Popular"
+                  ? popularMovies
+                  : rank === "Top rated"
+                  ? topRatedMovies
+                  : upComingMovies
+              }
+            />
+          </div>
+        );
+      })}
+      <Gallery movieList={movieList} />
+      <Pagination className="">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
 
-  )
+      <Footer></Footer>
+    </div>
+  );
 }
