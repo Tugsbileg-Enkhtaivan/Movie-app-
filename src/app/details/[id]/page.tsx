93c3e0729f8314c036920a948b16e7c08ee538b5
24LP7981 24LP7981 <24LP7981@G303-CC0146.local>
2025-03-21 10:25:51 +0800

    asdasda

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -1,44 +1,44 @@
-import { Moon, Star } from "lucide-react";
+// import { Moon, Star } from "lucide-react";
 
-export const Details = () => {
-  return (
-    <div>
-      {/* header */}
+// export const Details = () => {
+//   return (
+//     <div>
+//       {/* header */}
 
-      <h1>Wicked</h1>
-      <p>Release date</p>
-      <p>PG</p>
-      <p>Duration</p>
-      <h1>Rating</h1>
-      <Star></Star>
-      <p>Count of Reviews</p>
+//       <h1>Wicked</h1>
+//       <p>Release date</p>
+//       <p>PG</p>
+//       <p>Duration</p>
+//       <h1>Rating</h1>
+//       <Star></Star>
+//       <p>Count of Reviews</p>
 
-      {/* png */}
-      {/* trailer */}
+//       {/* png */}
+//       {/* trailer */}
 
-      {/* Genre list */}
+//       {/* Genre list */}
 
-      {/* Description */}
+//       {/* Description */}
 
-      <h1>Directer</h1>
+//       <h1>Directer</h1>
 
-      <h1> Writers</h1>
+//       <h1> Writers</h1>
 
-      <h1>Stars</h1>
+//       <h1>Stars</h1>
 
-      {/* More like this */}
+//       {/* More like this */}
 
-      {/* footer */}
-    </div>
-  );
-};
+//       {/* footer */}
+//     </div>
+//   );
+// };
 
-// Reminder dont forget Loading
-// Trailer work
-// API
-// More like this page (See more button) and Pagination
-// Night mode
-// Search bar and mini cards
-// Search loading and no result
-// Result page
-// Genre list
+// // Reminder dont forget Loading
+// // Trailer work
+// // API
+// // More like this page (See more button) and Pagination
+// // Night mode
+// // Search bar and mini cards
+// // Search loading and no result
+// // Result page
+// // Genre list
